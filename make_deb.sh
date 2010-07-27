af26a1a0c37e91c0e3469771de32adc68beb98d2
olivier <olivier@utopie>
2010-07-27 13:47:30 +0200

    wrong deb section

--- make_deb.sh
+++ make_deb.sh
@@ -1,4 +1,4 @@
 rm -r debian
-python setup.py --command-packages=stdeb.command debianize --package="icehms" --section="universe/devel" --depends="python-zeroc-ice" --maintainer="Olivier R-D <olivier@dummyaddress.com>"
+python setup.py --command-packages=stdeb.command debianize --package="icehms" --section="devel" --depends="python-zeroc-ice" --maintainer="Olivier R-D <olivier@dummyaddress.com>"
 echo '#!/bin/sh \npython /usr/bin/icehms_postinstall.py' > debian/icehms.postinst
 dpkg-buildpackage -rfakeroot -uc -us
