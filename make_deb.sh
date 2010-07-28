3efac8e38aa2e97ee97cfe673d802f03c049f18e
olivier <olivier@utopie>
2010-07-28 10:40:12 +0200

    details

--- make_deb.sh
+++ make_deb.sh
@@ -1,5 +1,6 @@
 rm -r debian
 #python setup.py --command-packages=stdeb.command debianize --package="icehms" --section="devel" --depends="python-zeroc-ice" --maintainer="Olivier R-D <olivier@dummyaddress.com>" --debian-version=2
+#python setup.py --command-packages=stdeb.command debianize  --workaround-548392=False #workaround if we want it to work with python < 2.6
 python setup.py --command-packages=stdeb.command debianize  
 echo '#!/bin/sh \npython /usr/bin/icehms_postinstall.py' > debian/icehms.postinst
 dpkg-buildpackage -rfakeroot -uc -us
