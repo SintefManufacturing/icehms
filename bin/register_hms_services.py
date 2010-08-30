57f0a1c615ebe2f28d73019a455384d466534882
olivier <olivier@utopie>
2010-08-30 15:26:12 +0200

    autodetect ice version for icebox configuration

--- bin/register_hms_services.py
+++ bin/register_hms_services.py
@@ -3,10 +3,16 @@ import os
 import sys
 import icehms
 
+#Big hack to find ice version
+v = str(  icehms.Ice.IcePy.intVersion() )
+version = v[0] + v[2]
+
 action = " add "
 if len(sys.argv) > 1:
     action = " update "
 
-cmd = 'icegridadmin --Ice.Default.Locator=IceGrid/Locator:"' +  icehms.IceRegistryServer + '" -e "application ' + action + icehms.iceboxpath + '"'
+#cmd = 'icegridadmin --Ice.Default.Locator=IceGrid/Locator:"' +  icehms.IceRegistryServer + '" -e "application ' + action + icehms.iceboxpath + '"'
+cmd = 'icegridadmin --Ice.Default.Locator=IceGrid/Locator:"%s" -e "application %s %s ice-version=%s"' % (icehms.IceRegistryServer, action, icehms.iceboxpath, version) 
+print cmd
 os.system(cmd)
 
