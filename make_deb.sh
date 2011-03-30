a058bdb8462ef8f44a0e85e2c3955c7846873c9a
Olivier <olivier@utopia>
2011-03-30 11:36:11 +0200

    add copying file add checks for required debian packages in make_Deb.sh

--- make_deb.sh
+++ make_deb.sh
@@ -1,11 +1,28 @@
-#!/bin/sh
-set -x
+#!/bin/bash
+#set -x
+
+function check_deb {
+
+    dpkg -s $1 > /dev/null
+    if [ `echo $?` != 0 ] ; then
+        echo "please install $1"
+        exit 1
+    fi
+}
+#we need stdeb
+check_deb python-stdeb
+check_deb build-essential
+
 #get version from setup.py file
 VERSION=`cat setup.py | grep "VERSION =" | sed s/'VERSION = '// | sed s/\"//g   ` 
 rm -r deb_dist
 python setup.py --command-packages=stdeb.command sdist_dsc
-echo '#!/bin/sh \npython /usr/bin/icehms_postinstall.py' > deb_dist/icehms-$VERSION/debian/icehms.postinst
+echo -e '#!/bin/sh \npython /usr/bin/icehms_postinstall.py' > deb_dist/icehms-$VERSION/debian/icehms.postinst
 cd deb_dist/icehms-$VERSION/
 dpkg-buildpackage -rfakeroot -uc -us
 cd .. 
-sudo dpkg -i icehms_${VERSION}-1_all.deb
+echo "\n Install package ?(Y/n)\n"
+read ANS
+if [ X"$ANS" = "Xy" ]; then 
+    sudo dpkg -i icehms_${VERSION}-1_all.deb
+fi
