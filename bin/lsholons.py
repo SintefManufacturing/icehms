8538b3ce303da19cdcba950f9f6741c66fa61d55
Olivier <olivier@utopia>
2011-09-01 11:24:57 +0200

    Make lsholons support holons outside f hms

--- bin/lsholons.py
+++ bin/lsholons.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 
 import icehms
+import Ice
 
 mgr = icehms.IceManager()
 mgr.initIce()
 try:
     objs = mgr.admin.getAllObjectInfos("*")
     print "Holons are :"
-    print "Holons are defined as object registered under ::hms:: namespace"
+    print "Holons are defined as object inherinting :hms::Holon"
     for obj in objs:
         #print obj
         #print dir(obj)
         #print obj.type
         holontype = str(obj.type)
-        if holontype.startswith("::hms::"):
-            print obj.proxy
+        try:
+            if obj.proxy.ice_isA("::hms::Holon"):
+                print obj.proxy
+        except Exception, why:
+            print obj.proxy, " seems dead: ", why
 finally:
     mgr.shutdown()
 
